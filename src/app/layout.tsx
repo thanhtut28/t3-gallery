9601826b73b60036279edd0595d623e5012f6b2e
Than Htut Aung <rday61062@gmail.com>
2024-07-09 15:16:02 +0700

    scaffold UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -9,12 +10,24 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+      <div>Sign In</div>
+    </nav>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body className="flex flex-col gap-4">
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+const mockUrls = [
+  "https://utfs.io/f/12382230-5ada-4fb3-8a01-e55620b7e47d-gby9ah.jpg",
+  "https://utfs.io/f/bb9defc3-d1fd-4963-a19e-a9a9d98e6fbb-qge93q.jpg",
+  "https://utfs.io/f/d90e7254-0e0c-44e9-bfe7-48f1a88c35b9-rvmt1.jpg",
+  "https://utfs.io/f/9eab3831-7d99-450a-af28-15403543c69b-ohierx.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img
+              src={image.url}
+              alt="image"
+              className="aspect-video object-cover"
+            />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
