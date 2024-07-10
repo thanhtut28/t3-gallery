2a234f0cf942cb751787e47c924834fc64338847
Than Htut Aung <rday61062@gmail.com>
2024-07-10 21:44:47 +0700

    Added auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/12382230-5ada-4fb3-8a01-e55620b7e47d-gby9ah.jpg",
-  "https://utfs.io/f/bb9defc3-d1fd-4963-a19e-a9a9d98e6fbb-qge93q.jpg",
-  "https://utfs.io/f/d90e7254-0e0c-44e9-bfe7-48f1a88c35b9-rvmt1.jpg",
-  "https://utfs.io/f/9eab3831-7d99-450a-af28-15403543c69b-ohierx.jpg",
-];
+// const mockUrls = [
+//   "https://utfs.io/f/12382230-5ada-4fb3-8a01-e55620b7e47d-gby9ah.jpg",
+//   "https://utfs.io/f/bb9defc3-d1fd-4963-a19e-a9a9d98e6fbb-qge93q.jpg",
+//   "https://utfs.io/f/d90e7254-0e0c-44e9-bfe7-48f1a88c35b9-rvmt1.jpg",
+//   "https://utfs.io/f/9eab3831-7d99-450a-af28-15403543c69b-ohierx.jpg",
+// ];
 
-const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
+// const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
 
 export default async function HomePage() {
   const images = await db.query.images.findMany({
